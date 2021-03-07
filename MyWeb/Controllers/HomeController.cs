b3d8209e34907ecd94cd0d744037f268d991a471
Elizabeth Khong <ekorigamer@gmail.com>
2021-03-07 10:33:48 -0800

    Update

--- MyWeb/Controllers/HomeController.cs
+++ MyWeb/Controllers/HomeController.cs
@@ -35,6 +35,10 @@ namespace MyWeb.Controllers
         {
             return View();
         }
+        public IActionResult Geeksign()
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
